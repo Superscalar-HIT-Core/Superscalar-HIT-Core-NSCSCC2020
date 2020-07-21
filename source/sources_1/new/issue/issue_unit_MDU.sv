df82650cba61ed061d38dafa4a08c68df725585b
Bohan Hu <hubohancser@gmail.com>
2020-07-21 23:02:21 +0800

    add support for mdu busy

--- source/sources_1/new/issue/issue_unit_MDU.sv
+++ source/sources_1/new/issue/issue_unit_MDU.sv
@@ -217,7 +217,7 @@ iq_mdu u_iq_mdu(
 
 assign ready = ~full;   // 如果满了，则不能继续接受
 wire [`MDU_QUEUE_LEN-1:0] mul_valid_mask = mul_busy ? ~isMul_vec : isMul_vec;
-wire [`MDU_QUEUE_LEN-1:0] div_valid_mask = div_busy & isMul_vec : ~isMul_vec;
+wire [`MDU_QUEUE_LEN-1:0] div_valid_mask = div_busy ? isMul_vec : ~isMul_vec;
 
 wire [`MDU_QUEUE_LEN-1:0] arbit_vec = ready_vec & valid_vec & mul_valid_mask & div_valid_mask;
 
