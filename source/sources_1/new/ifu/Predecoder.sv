fe58138e069c18488352549e0aa0e3eb68402b76
Bohan Hu <hubohancser@gmail.com>
2020-08-05 04:29:13 +0800

    add RAS

--- source/sources_1/new/ifu/Predecoder.sv
+++ source/sources_1/new/ifu/Predecoder.sv
@@ -9,6 +9,8 @@ module Predecoder(
     output  logic           isJ,
     output  logic           isBr,
     output  logic           jr,
+    output  logic           isLink,
+    output  logic           isReturn,
     output  logic [31:0]    target
 );
 
@@ -26,12 +28,12 @@ module Predecoder(
                     jr      = `FALSE;
                     target  = {pc[31:28], inst[25:0], 2'b00};
                 end
-                // `BAL: begin  // BAL
-                //     isJ     = `TRUE;    // must redirect, with different address
-                //     isBr    = `FALSE;
-                //     jr      = `TRUE;
-                //     target  = pc + {{14{inst[15]}}, inst[15:0] << 2};
-                // end
+                `BAL, `B: begin  // BAL
+                    isJ     = `TRUE;    // must redirect, with different address
+                    isBr    = `FALSE;
+                    jr      = `FALSE;
+                    target  = pc + {{14{inst[15]}}, inst[15:0], 2'b00} + 4;
+                end
                 // backend-redirect, predecode for wrong target or checking NLP/BPD false postive(br/normal mispredict)
                 `BEQ, `BNE, `BGEZ, `BGEZAL, `BGTZ, `BLEZ, `BLTZ, `BLTZAL: begin
                     isJ     = `FALSE;  // redirect unknown;
@@ -50,6 +52,39 @@ module Predecoder(
                     isBr    = `FALSE;
                     jr      = `FALSE;   // don't redirect on normal instrutions
                     target  = 32'h00000000;
+                end
+            endcase
+        end
+    end
+
+    always_comb begin
+        isLink = `FALSE;
+        if(valid) begin
+            casez (inst)
+                // quick redirect
+                `JAL,`BGEZAL, `BLTZAL: begin
+                    isLink = `TRUE;
+                end
+                `JALR:  begin
+                    if(inst[15:11] == 5'h1f) isLink = `TRUE;
+                end
+                default: begin
+                    isLink = `FALSE;
+                end
+            endcase
+        end
+    end
+
+    always_comb begin
+        isReturn = `FALSE;
+        if(valid) begin
+            casez (inst)
+                // quick redirect
+                `JR, `JALR: begin
+                    if(inst[25:21] == 5'h1f) isReturn = `TRUE;
+                end
+                default: begin
+                    isReturn = `FALSE;
                 end
             endcase
         end
---
+++ source/sources_1/new/ifu/RAS.sv
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+`include "../defs.sv"
+`include "../defines/defines.svh"
+
+module RAS(
+    input   logic [31:0]    pc,
+    input                   recover,
+    input   logic           valid,
+    input   logic           isLink,
+    input   logic           isReturn,
+    output  logic [31:0]    target
+    input   logic           commit_valid,
+    input   logic           committed_isLink,
+    input   logic           committed_isReturn,
+    input   logic [31:0]    committed_pc
+);
+
+    reg [31:0] ras [15:0];  // 16 Depth ras
+    reg [3:0] ras_top, committed_ras_top;
+    always @(posedge clk)   begin
+        if(rst) begin
+            for(integer i=0;i<16;i++)   begin
+                ras[i] <= 0;
+            end
+            ras_top <= 0;
+        end else if(recover)    begin
+            for(integer i=0;i<16;i++)   begin
+                ras[i] <= committed_ras[i];
+            end
+            ras_top <= committed_ras_top;
+        end else if(valid && isLink)    begin
+            ras[ras_top] <= pc + 32'h8;
+            ras_top <= ras_top + 1;
+        end else if(valid && isReturn)  begin
+            ras_top <= ras_top - 1;
+        end
+    end
+
+    assign target = ras[ras_top];
+    
+    reg [31:0] committed_ras [15:0];  // 16 Depth ras(committed)
+
+    always @(posedge clk)   begin
+        if(rst) begin
+            for(integer i=0;i<16;i++)   begin
+                committed_ras[i] <= 0;
+            end
+            committed_ras_top <= 0;
+        end else if(commit_valid && committed_isLink)    begin
+            committed_ras[committed_ras_top] <= pc + 32'h8;
+            committed_ras_top <= committed_ras_top + 1;
+        end else if(commit_valid && committed_isReturn)  begin
+            committed_ras_top <= committed_ras_top - 1;
+        end
+    end
+
+    
+endmodule
